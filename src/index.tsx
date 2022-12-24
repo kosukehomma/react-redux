bca0a04807aed3e71b77bbf59599ea40f7624fe5
kosukehomma <kou4321ske@gmail.com>
2022-12-24 11:39:59 +0900

    first-commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+function App() {
+  return <h1>Hello React!</h1>
+}
+
+const container = document.getElementById('app')
+if (container) {
+  const root = createRoot(container)
+  root.render(<App />)
+}
